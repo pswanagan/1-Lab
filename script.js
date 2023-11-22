24e8bd61df58a4b364556374714c794e66ef61e2
pswanagan <pswanagan@gmail.com>
2023-11-22 09:44:59 -0600

    Completed section 1

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-console.log(isSum50);
+console.log("The numbers add up to 50: " + isSum50);
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-console.log(isTwoOdd);
+console.log("Are at least two of the numbers odd: " + isTwoOdd);
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-console.log(isOver25);
+console.log("No numbers are over 25: " + isOver25);
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-console.log(isUnique);
+console.log("All numbers are unique: " + isUnique);
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 // Finally, log the results.
-console.log(isValid);
+console.log("The numbers are valid: " + isValid);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -42,3 +42,16 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  /* Check if all numbers are divisible by 5. Cache the result in a variable.   */
+
+  const is5Divisible = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) >= 1;
+  console.log("All numbers are divisible by 5: " + is5Divisible);
+  // Check if the first number is larger than the last.
+ const isFirstLarger = n1 > n4;
+ console.log("Is the first number larger than the last number: ")
+  /* Accomplish the following arithmetic chain:
+Subtract the first number from the second number.
+Multiply the result by the third number.
+Find the remainder of dividing the result by the fourth number. */
+
