f7d0caf9fca981df349205dac6aa905f2ec5a204
pswanagan <pswanagan@gmail.com>
2023-11-25 07:42:54 -0600

    completed section 2 55mph equations

--- script.js
+++ script.js
@@ -59,3 +59,40 @@ const endingAnswer = (n2 - n1) * n3 / n4;
 
 console.log("The answer to the arithmetic chain is: " + endingAnswer);
 
+
+/**
+ * Part 2
+ * 
+ * You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+How many gallons of fuel will you need for the entire trip?
+Will your budget be enough to cover the fuel expense?
+How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+Log the results of your calculations using string concatenation or template literals.
+ */
+
+const miles55 = 1500 / 30;
+console.log(`At 55 miles per hour it will take ${miles55} gallons of gas to complete the trip.`);
+
+const fuel55 = (miles55 * 3) < 175.
+console.log(`Will the fuel budget cover the trip: ${fuel55}.`); 
+
+const time55 = 1500 / 55;
+console.log(`It will take ${time55} hours to complete trip at 55mph.`)
+
+const miles60 = 1500 / 28;
+console.log(`At 55 miles per hour it will take ${miles60} gallons of gas to complete the trip.`);
+
+const fuel60 = (miles60 * 3) < 175.
+console.log(`Will the fuel budget cover the trip: ${fuel60}.`); 
+
+const time60 = 1500 / 60;
+console.log(`It will take ${time55} hours to complete trip at 55mph.`)
